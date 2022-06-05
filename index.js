e2d2336cd3a627c9a0ba1935e4fb334058633105
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-06-05 18:04:03 +0700

    haha

--- index.js
+++ index.js
@@ -175,9 +175,7 @@ app.get("/:type", (req, res) => {
     fill(req, res);
 });
 
-const port = process.env.PORT || '5000';
-
-app.listen(port, () => {
+app.listen(5000, () => {
     console.log(`Server is running ${port}`);
 });
 
